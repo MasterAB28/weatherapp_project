422fbc7743d52b7527eba36c2fa23d1b17cafdc0
aviad.barel <aviad0909@gmail.com>
2024-02-06 16:35:48 +0200

    fix tests

--- test_selenium.py
+++ test_selenium.py
@@ -18,7 +18,6 @@ class TestSelenium(unittest.TestCase):
         options.add_argument('--ignore-ssl-errors=yes')
         options.add_argument('--ignore-certificate-errors')
         options.add_argument('--timeout=120')
-        options.add_argument('--no-sandbox')
         cls.driver = webdriver.Remote(command_executor='http://selenium:4444/wd/hub', options=options)
 
     @classmethod
