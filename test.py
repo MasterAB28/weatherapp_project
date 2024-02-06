0adc9d3a846c87d2f3e45a4b6b230f93cc0b8382
aviad.barel <aviad0909@gmail.com>
2024-02-06 08:38:22 +0200

    change tests

--- test.py
+++ test.py
@@ -9,15 +9,10 @@ import os
 
 
 class TestWebsiteReachable(unittest.TestCase):
-    def setUp(self):
-        os.system("docker compose -f /home/aviad/Git/devops/containers-docker/compose.yaml up -d")
 
     def test_website_reachable(self):
         status_code = requests.get("http://172.17.0.1:8000/").status_code
         self.assertEqual(status_code, 200, "OOPS the website is not reachable")
-
-    def tearDown(self):
-        os.system("docker compose -f /home/aviad/Git/devops/containers-docker/compose.yaml kill")
 
 
 if __name__ == "__main__":
