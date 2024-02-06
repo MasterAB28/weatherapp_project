2432ce628d7c1a6798c41ba2d7cf1a338f183f9a
aviad.barel <aviad0909@gmail.com>
2024-02-06 18:08:10 +0200

    change dir

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 Author: Aviad Barel
 Reviewer: Tomer
 """
-
+import time
 import unittest
 import requests
 import os
@@ -11,6 +11,7 @@ import os
 class TestWebsiteReachable(unittest.TestCase):
 
     def test_website_reachable(self):
+        time.sleep(30)
         status_code = requests.get("http://172.17.0.1:8000/").status_code
         self.assertEqual(status_code, 200, "OOPS the website is not reachable")
 
