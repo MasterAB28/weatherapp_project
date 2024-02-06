a8061f746df278ed52e74af2948a74ca54dd52e5
aviad.barel <aviad0909@gmail.com>
2024-02-06 18:25:34 +0200

    change dir

--- test.py
+++ test.py
@@ -11,11 +11,6 @@ import os
 class TestWebsiteReachable(unittest.TestCase):
 
     def test_website_reachable(self):
-<<<<<<< HEAD
-        
-=======
-        time.sleep(30)
->>>>>>> d14501c800ae5826bd8c2abe45c8cdc5fb68df86
         status_code = requests.get("http://172.17.0.1:8000/").status_code
         self.assertEqual(status_code, 200, "OOPS the website is not reachable")
 
