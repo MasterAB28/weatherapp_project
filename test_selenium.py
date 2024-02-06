489adade58c03799c5fe5764fec92f5e78dfe7f2
aviad.barel <aviad0909@gmail.com>
2024-02-06 15:38:59 +0200

    fix tests

--- test_selenium.py
+++ test_selenium.py
@@ -9,12 +9,18 @@ from selenium.webdriver.common.by import By
 
 
 class TestSelenium(unittest.TestCase):
-    def setUp(self):
+    driver = None
+    @classmethod
+    def setUpClass(cls):
         options = webdriver.ChromeOptions()
         options.add_argument('--ignore-ssl-errors=yes')
         options.add_argument('--ignore-certificate-errors')
         options.add_argument('--timeout=120')
-        self.driver = webdriver.Remote(command_executor='http://selenium:4444', options=options)
+        cls.driver = webdriver.Remote(command_executor='http://selenium:4444/wd/hub', options=options)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
 
     def test_positive(self):
         self.driver.get("http://172.17.0.1:8000/")
@@ -22,7 +28,6 @@ class TestSelenium(unittest.TestCase):
         driver.find_element(by=By.NAME, value="city").send_keys("Ashdod")
         driver.find_element(by=By.CSS_SELECTOR, value="button").click()
         self.assertTrue(driver.find_element(By.NAME, "found").is_enabled())
-        driver.quit()
 
     def test_negative(self):
         self.driver.get("http://172.17.0.1:8000/")
@@ -30,10 +35,6 @@ class TestSelenium(unittest.TestCase):
         driver.find_element(by=By.NAME, value="city").send_keys("Gaza")
         driver.find_element(by=By.CSS_SELECTOR, value="button").click()
         self.assertTrue(driver.find_element(By.NAME, "not-found").is_enabled())
-        driver.quit()
-
-    def tearDown(self):
-        self.driver.close()
 
 
 if __name__ == '__main__':
