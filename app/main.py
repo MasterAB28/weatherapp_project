4a9c780da4bc1f5257c5132089f7e2202ebe4acf
Administrator <aviad0909@gmail.com>
2024-02-17 15:15:56 +0200

    add region

--- app/main.py
+++ app/main.py
@@ -8,18 +8,18 @@ from API_functions import get_weather, download_image, dynamodb_send_item
 
 app = Flask(__name__)
 
-weather = {}
+city = None
 
 
 @app.route("/", methods=['GET', 'POST'])
 def search():
+    global city
     # ch    eck if the request is get to return output without a data
     if request.method == 'GET':
         return render_template('mainpage.html', method='get')
     # check if the request is post to return an output with data
     if request.method == 'POST':
         city = request.form['city']
-        global weather
         weather = get_weather(city)
         return render_template('mainpage.html', **weather, success='', method='post')
 
@@ -31,7 +31,7 @@ def download():
 
 @app.route("/dynamodb")
 def dynamodb():
-    return dynamodb_send_item(weather)
+    return dynamodb_send_item(get_weather(city))
 
 
 if __name__ == "__main__":
