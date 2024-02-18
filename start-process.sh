3dac258e5474a18aa25510b1df1a0c619fcb370b
Administrator <aviad0909@gmail.com>
2024-02-18 16:43:31 +0200

    add deploy id

--- start-process.sh
+++ start-process.sh
@@ -12,6 +12,7 @@ docker pull public.ecr.aws/v7l9w0u1/weather_app:latest
 docker image tag public.ecr.aws/v7l9w0u1/nginx:latest aviadbarel/nginx:latest
 docker image tag public.ecr.aws/v7l9w0u1/weather_app:latest aviadbarel/weather_app:latest
 echo "hi"
-pwd
 
-docker-compose up -d
+deploy=$(aws deploy list-deployments --application-name weather_app --deployment-group-name weather_app | jq -r '.deployments[0]')
+
+docker-compose --project-directory "/opt/codedeploy-agent/deployment-root/721aa903-fe9d-42ea-8d40-00fa8d155ddd/$deploy/deployment-archive/" up -d
