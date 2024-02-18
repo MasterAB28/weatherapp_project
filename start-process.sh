552e6c540f03158787c27b2e3e1cd68546003b4f
Administrator <aviad0909@gmail.com>
2024-02-18 15:18:41 +0200

    add docker rm and down

--- start-process.sh
+++ start-process.sh
@@ -2,6 +2,10 @@
 
 aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/v7l9w0u1
 
+docker-compose down
+
+docker image rm -f public.ecr.aws/v7l9w0u1/nginx:latest aviadbarel/nginx:latest public.ecr.aws/v7l9w0u1/weather_app:latest aviadbarel/weather_app:latest
+
 docker pull public.ecr.aws/v7l9w0u1/nginx:latest
 docker pull public.ecr.aws/v7l9w0u1/weather_app:latest
 
