19cf50a33e3c8b4b26f99aef1ecd7ade178f6b22
Administrator <aviad0909@gmail.com>
2024-02-18 12:27:20 +0200

    add appsepc.yml

---
+++ start-process.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/v7l9w0u1
+
+docker pull public.ecr.aws/v7l9w0u1/nginx:latest
+docker pull public.ecr.aws/v7l9w0u1/weather_app:latest
+
+docker image tag public.ecr.aws/v7l9w0u1/nginx:latest aviadbarel/nginx:latest
+docker image tag public.ecr.aws/v7l9w0u1/weather_app:latest aviadbarel/weather_app:latest
+
+docker compose up -d
