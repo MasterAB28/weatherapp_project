60471e910f172ea7cd7d34c896ecc2c0173af954
aviad.barel <aviad0909@gmail.com>
2024-03-16 18:43:22 +0200

    change Dockerfile

--- tests/test.py
+++ tests/test.py
@@ -11,7 +11,7 @@ import os
 class TestWebsiteReachable(unittest.TestCase):
 
     def test_website_reachable(self):
-        status_code = requests.get("http://172.17.0.1/").status_code
+        status_code = requests.get("http://172.17.0.1:8000").status_code
         self.assertEqual(status_code, 200, "OOPS the website is not reachable")
 
 
